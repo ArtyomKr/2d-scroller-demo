6a70cff3a6f5befcb21811edee1759c7714f0d47
ArtyomKr <tyomik.krasnitsky@gmail.com>
2022-11-09 12:18:44 +0300

    feat: make fireball face moving direction

--- scenes/Objects/FireBall.gd
+++ scenes/Objects/FireBall.gd
@@ -1,6 +1,10 @@
 extends RigidBody2D
 
 
+func _physics_process(delta):
+	$AnimatedSprite.flip_h = linear_velocity.x > 0
+
+
 func _on_FireBall_body_entered(body):
 	if body is Player:
 		body.damage()
