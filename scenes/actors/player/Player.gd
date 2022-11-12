2d5aa74e7bd136ff632e7857af3d82cb72378010
ArtyomKr <tyomik.krasnitsky@gmail.com>
2022-11-12 22:08:08 +0300

    feat: changed AnimatedSprite to AnimationPlayer, rewrote most state managment logic

--- scenes/actors/player/Player.gd
+++ scenes/actors/player/Player.gd
@@ -10,11 +10,14 @@ export var speed = 300
 export var jump_force = 500
 
 enum STATE {
-	MOVING,
+	BLOCKED,
+	WALKING,
+	JUMPING,
+	IN_AIR,
 	IDLE,
 	DEAD,
 	ATTACKING,
-	IS_HIT
+	HURT
 }
 
 var screen_size
@@ -31,45 +34,32 @@ func _ready():
 
 
 func _physics_process(delta):
-	var direction = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
-	var movement_speed = direction * speed
-	
-	velocity.x = movement_speed * delta * 100
-	
-	if is_on_floor() && Input.is_action_just_pressed('jump'):
-		velocity.y -= jump_force
-		
-	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
-	
-	monitor_attack_input()
-	manage_state()
-	play_animation()
+	manage_state(delta)
+	print_debug(_state)
 
 
 func start(pos: Vector2):
 	position = pos
 	show()
-	$CollisionShape2D.disabled = false
+	$HitBox.disabled = false
 
 
 func die():
 	_state = STATE.DEAD
-	$CollisionShape2D.set_deferred('disabled', true)
+	$HitBox.set_deferred('disabled', true)
 	get_tree().paused = true
 	
-	$AnimatedSprite.pause_mode = Node.PAUSE_MODE_PROCESS
-	$AnimatedSprite.play('die')
-	yield($AnimatedSprite, "animation_finished")
-	$AnimatedSprite.stop()
+	$AnimationPlayer.pause_mode = Node.PAUSE_MODE_PROCESS
+	$AnimationPlayer.play('die')
+	yield($AnimationPlayer, "animation_finished")
+	$AnimationPlayer.stop()
 	
 	hide()
 	emit_signal('player_died')
 
 
 func damage():
-	if $HitTimer.is_stopped():
+	if $HitTaimer.is_stopped():
 		$HitTimer.start()
 		hp -= 1
 		emit_signal('hit', hp)
@@ -91,17 +81,17 @@ func play_animation():
 		if velocity.y < -400:
 			current_animation = 'jump'
 
-	if _state == STATE.IS_HIT:
+	if _state == STATE.HURT:
 		current_animation = 'hurt'
 	
 	if _state == STATE.ATTACKING:
 			current_animation = 'attack'
 
 	if abs(velocity.x) > 0.1:
-		$AnimatedSprite.flip_h = velocity.x < 0
+		$AnimationPlayer.flip_h = velocity.x < 0
 	
-	if current_animation != $AnimatedSprite.animation:	
-		$AnimatedSprite.play(current_animation)
+	if current_animation != $AnimationPlayer.animation:	
+		$AnimationPlayer.play(current_animation)
 
 
 func monitor_attack_input():
@@ -112,19 +102,57 @@ func monitor_attack_input():
 			$AttackTimer.start()
 
 
-func manage_state():
-	if (abs(velocity.x) > 0 || abs(velocity.y) > 0):
-		_state = STATE.MOVING
-	elif (velocity == Vector2.ZERO):
-		_state = STATE.IDLE
+func manage_state(delta):
+	var current_animation: String
+	var direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	var movement_speed = direction * speed
 		
-	if !$AttackTimer.is_stopped():
-		_state = STATE.ATTACKING
-		
-	if !$HitTimer.is_stopped():	
-		_state = STATE.IS_HIT
+	
+	match _state:
+		STATE.BLOCKED:
+			pass
+		STATE.IDLE:
+			if (is_on_floor() && abs(direction) > 0):
+				_state = STATE.WALKING
+			if Input.is_action_just_pressed("attack"):
+				_state = STATE.ATTACKING
+			if !is_on_floor():
+				_state = STATE.IN_AIR
+			current_animation = "idle_0"
+			pass
+		STATE.WALKING:
+			velocity.x = movement_speed * delta * 100	
+			velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+			current_animation = "run"
+			
+			if Input.is_action_just_pressed("attack"):
+				_state = STATE.ATTACKING
+			if is_on_floor() && Input.is_action_just_pressed("jump"):
+				velocity.y -= jump_force
+				_state = STATE.IN_AIR
+			if velocity.length() < 1:
+				_state = STATE.IDLE
+			pass
+		STATE.IN_AIR:
+			velocity.x = movement_speed * delta * 100
+			velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+			
+			if is_on_floor():
+				_state = STATE.IDLE
+			current_animation = "jump"
+			pass
+	
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+				
+	if abs(velocity.x) > 0.1:
+		$Sprite.flip_h = velocity.x < 0
+	
+	if current_animation != $AnimationPlayer.assigned_animation:	
+		$AnimationPlayer.play(current_animation)
 
-
+func goto_idle():
+	_state = STATE.IDLE
 
 func _on_HitDetector_body_entered(body):
 	if body is Enemy:
