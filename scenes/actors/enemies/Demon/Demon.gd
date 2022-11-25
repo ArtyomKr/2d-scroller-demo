dd8d28d8c938d94aebb48aeb23b0858985a15bfa
ArtyomKr <tyomik.krasnitsky@gmail.com>
2022-11-25 12:58:49 +0300

    feat: add state managment code & alert timer to demon enemy

--- scenes/actors/enemies/Demon/Demon.gd
+++ scenes/actors/enemies/Demon/Demon.gd
@@ -2,12 +2,27 @@ extends Enemy
 
 
 signal detected_player
+
+
 export var speed = 300
 export var jump_force = 500
 
+enum STATE {
+	TELEPORTING,
+	IN_AIR,
+	IDLE,
+	ALERTED,
+	DEAD,
+	ATTACKING,
+	HURT
+}
+
 var is_attacking = false
+var target
+var _state = STATE.IDLE
 const FireBall = preload("res://scenes/Objects/FireBall.tscn")
 
+ 
 func _init():
 	hp = 10
 
@@ -48,10 +63,68 @@ func get_animation():
 	$AnimatedSprite.play(current_animation)
 
 
+func manage_state(delta):
+	var current_animation: String
+	
+	match _state:
+		STATE.IDLE:
+			velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+
+			current_animation = "idle"
+			pass
+		STATE.ALERTED:
+			velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+			
+			if target:
+				_state = STATE.ATTACKING
+				
+			current_animation = "idle"
+			pass
+		STATE.TELEPORTING:
+			velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+			current_animation = "teleport"
+			
+			pass
+		STATE.IN_AIR:
+			velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+			
+			if is_on_floor(): 
+				_state = STATE.IDLE
+			if is_on_floor() && abs(velocity.x) > 0:
+				_state = STATE.TELEPORTING
+
+			pass
+		STATE.ATTACKING:
+			velocity.x = 0
+			
+			if target:
+				attack(target)
+			else:
+				 _state = STATE.ALERTED
+				
+			current_animation = "attack"
+		STATE.HURT:
+			current_animation = "hurt"
+		STATE.DEAD:
+			get_tree().paused = true
+			$AnimationPlayer.pause_mode = Node.PAUSE_MODE_PROCESS
+			$AnimationPlayer.play("die")
+			yield($AnimationPlayer, "animation_finished")
+			hide()
+	
+	if current_animation != $AnimationPlayer.assigned_animation:	
+		$AnimationPlayer.play(current_animation)
+
+
+func goto_idle():
+	_state = STATE.IDLE
+
+
 func _on_DetectionArea_body_entered(body):
 	if body is Player:
 		emit_signal("detected_player")
-		attack(body)
+		target = body
+		attack(target)
 
 
 func attack(target: Node):
@@ -65,7 +138,3 @@ func attack(target: Node):
 	fireball.set_as_toplevel(true)
 	
 	call_deferred("add_child", fireball)
-
-
-func _on_HurtBox_area_entered(area):
-	pass # Replace with function body.
